cb68b4520d7b96060760ba4340550b0077794d56
LeonHuang <nd19234643@gmail.com>
2018-03-26 13:41:49 +0800

    Update by Leon, 20180326
    1. 優化程式碼
    2. 解決 GPS 資料遺失的問題

--- 20180213_ADAS_DVR2_DIR.X/main.c
+++ 20180213_ADAS_DVR2_DIR.X/main.c
@@ -26,157 +26,320 @@
 
   //  void interrupt  ISR                 (void);
 
-unsigned char Flow_Init = 0;
-unsigned char counter= 0;
-int sensorVal_1= 0;
-float lowValueOfFrequency = 0.6; // unit: Hz
-float highValueOfFrequency = 1.5; // unit: Hz
-  
-unsigned char Period[3]= {0, 0, 0};
-unsigned char Pos[2]= {0, 0};
-unsigned char Neg[2]= {0, 0};
-void checkFrequencyRange(float , float , int);
+// {LeonHuang20180313+ [Optimization] 
+#define SIZE 208 // unit: byte 
+#define DELAY_TIME 20 // unit: ms 
 
-unsigned char GPS_flag= 0, GPS_out= 0;
-unsigned char GPS[100]={0};
-unsigned char test=0;
+// Optimization 
+//float lowFrequency = 0.6; // unit: Hz
+//float highFrequency = 1.5; // unit: Hz
+#define LOWER_BOUND 33 // ((1 / lowFrequency) * 1000 / DELAY_TIME);
+#define UPPER_BOUND 83 // ((1 / highFrequency) * 1000 / DELAY_TIME);
 
+/*
+unsigned char Period[3] = {0, 0, 0};
+unsigned char Pos[2] = {0, 0};
+unsigned char Neg[2] = {0, 0};
+*/
+//unsigned char sensorVal_1 = 0;
 
+unsigned char tempPositiveVal = 0, tempNegativeVal = 0;
+unsigned char positiveVal = 0, negativeVal = 0;
+
+unsigned char highestPriority = 0;
+unsigned char outputCount = 0, freqCount = 0;
+short int timerCount = 0;
+short int rIndex = 0, rHead = 0;
+short int wIndex = 0, wHead = 0;
+unsigned char buffer[SIZE] = {0};
+
+// Method Prototype 
+void initTimer0();
+void checkFrequencyRange();
+// LeonHuang20180313-}
+
+/* GPS Format 
+ $GP GGA 
+ $GP GLL 
+ $GP GSA 
+ $GP GSV 
+ $GP MSS 
+ $GP RMC 
+ $GP VTG 
+ $GP ZDA 
+ */
 void interrupt InterruptHandlerLow ()
 {
-  unsigned char cUART_char= 0;
-  
-  if (PIR1bits.RCIF==1)//is interrupt occured by EUSART receive?,
-                        //then RCREG is full we have new data (cleared when RCREG is read)
-  {
-  // if(RCSTA&0x06) //more efficient way than following commented method to check for reception error
-    if(RCSTAbits.FERR==1 || RCSTAbits.OERR==1 )
-    {
-      RCSTAbits.CREN=0;    //Overrun error (can be cleared by clearing bit CREN)
-      cUART_char=RCREG;    //clear Framing error
-      RCSTAbits.CREN=1;
-    }
-    else
-    {
-       cUART_char = RCREG; // read new data into variable
-    }
-    
-  if(GPS_out==0){  
-    if( cUART_char == '$' )
-    {
-        GPS_flag= 0;
-        GPS_out= 0;
-        GPS[GPS_flag]= '$';
-        GPS_flag = GPS_flag +1;
-    }
-    else if( GPS_flag > 0 )
-    {
-        test=1;
-        GPS[GPS_flag]= cUART_char;
-     
-        if(GPS[GPS_flag]==0x0A)
-            GPS_out= 1;  
-        GPS_flag = GPS_flag +1;
-    }
-    else
-        ;
-    
-    if( GPS_flag==6 )
-    {
-        if( (GPS[5]=='C') || (GPS[5]=='A') )
-            ;
+    unsigned char cUART_char = 0;
+
+    if (PIR1bits.RCIF == 1) // is interrupt occured by EUSART receive?,
+    {                               // then RCREG is full we have new data (cleared when RCREG is read)
+                               
+        // if(RCSTA&0x06) //more efficient way than following commented method to check for reception error
+        if (RCSTAbits.FERR == 1 || RCSTAbits.OERR == 1)
+        {
+            RCSTAbits.CREN = 0;    //Overrun error (can be cleared by clearing bit CREN)
+            cUART_char = RCREG;    //clear Framing error
+            RCSTAbits.CREN = 1;
+        }
         else
         {
-            GPS_flag= 0;
-            GPS_out= 0;
+            cUART_char = RCREG; // read new data into variable
         }
-    
+        
+        // {LeonHuang20180313+ [Read GPS Data] 
+        if (highestPriority == 1)
+        {
+            rIndex = 0;
+            buffer[rHead] = 0;
+            return;
+        }
+        
+        if ((rHead + rIndex + 1) % SIZE == wHead) // Buffer is full 
+        {
+            rIndex = 0;
+            buffer[rHead] = 0;
+            return;
+        }
+        
+        // Head of GPS
+        if (cUART_char == '$')
+        {
+            rIndex = 1;
+            buffer[rHead] = cUART_char;
+            
+            return;
+        }
+        
+        // {GPS Data Filter+ 
+        if (buffer[rHead] == '$' && rIndex == 3 && cUART_char == 'V') // $GPVTG,
+        {
+            rIndex = 0;
+            buffer[rHead] = 0;
+            return;
+        }
+        else if (buffer[rHead] == '$' && rIndex == 3 && cUART_char == 'Z')  // $GPZDA
+        {
+            rIndex = 0;
+            buffer[rHead] = 0;
+            return;
+        }
+        else if (buffer[rHead] == '$' && rIndex == 3 && cUART_char == 'M')  // $GPMSS
+        {
+            rIndex = 0;
+            buffer[rHead] = 0;
+            return;
+        }
+        else if (buffer[rHead] == '$' && rIndex == 4 && cUART_char == 'S') // $GPGSA, $GPGSV,
+        {
+            rIndex = 0;
+            buffer[rHead] = 0;
+            return;
+        }
+        else if (buffer[rHead] == '$' && rIndex == 4 && cUART_char == 'L') // $GPGLL
+        {
+            rIndex = 0;
+            buffer[rHead] = 0;
+            return;
+        }
+        else if (buffer[rHead] == '$') // $GPGGA, $GPRMC,     We want 
+        {
+            buffer[(rHead + rIndex) % SIZE] = cUART_char;
+            
+            //  End of GPS 
+            if (cUART_char == 0x0A) // Optimization 
+            {
+                outputCount++;
+                rHead = (rHead + rIndex + 1) % SIZE;
+                rIndex = 0;
+                return;
+            }
+            
+            rIndex++;
+        }
+        else
+        {
+            // do nothing 
+        }
+        // GPS Data Filter-} 
+        // LeonHuang20180313-}
+               
+        // {LeonHuang20180313+ [ Original code, hot key: Ctrl + / ] 
+//        if (outputFlag == 0)
+//        {  
+//            if (cUART_char == '$' )
+//            {
+//                index = 0;
+//                buffer[index] = '$';
+//                index++;
+//            }
+//            else if (index > 0 )
+//            {
+//                buffer[index] = cUART_char;
+//     
+//                if (buffer[index] == 0x0A) //  new line
+//                    outputFlag = 1;  
+//                index++;
+//            }
+//            else
+//            {}
+//                //; // ?
+//           
+//            if (index == 6) // ,
+//            {
+//                if ((buffer[5] == 'C') || (buffer[5] == 'A'))
+//                {}
+//                else
+//                {
+//                    index = 0;
+//                    outputFlag = 0;
+//                }
+//            }
+//        }
+        // LeonHuang20180313-}
     }
-
-  }
-  }
 }
-
 
 void main(void)
 {
-    unsigned char i= 0, j=0, k=0;
+//    unsigned char i = 0;
     
     __delay_ms(100); 
     
     Example_System_Init();    
-	EUSARTInit();
+    EUSARTInit();
             
     //TMR2ON =1;
-   // INTCONbits.GIE = 1;             // Initialization complete. Begin servicing interrupts.
+    // INTCONbits.GIE = 1;             // Initialization complete. Begin servicing interrupts.
 
-      // Initial Information
-  if (Flow_Init == 0)
-  {
-    __delay_ms(1000);  
-    Flow_Init = 1;
-  }
+    // {LeonHuang20180319+ [Init Timer0] 
+    initTimer0();
+    // test Timer0, use oscilloscope to analyze, cycle: 20 ms   
+//    while (1)
+//    {        
+//        if (TMR0IF) 
+//        {
+//            timerCount++;
+//            if (timerCount == 313) // 312 + 1, 20 ms 
+//            {
+//                putch('A');
+//                timerCount = 0;
+//                TMR0 = 126; //  128 - 2
+//            }
+//            TMR0IF = 0;
+//        }
+//    }
+    // LeonHuang20180319-}
+    
+    // Wait some time 
+    __delay_ms(100);
 
-	while(1)
-	{
-
-        if( GPS_out == 1)
+    while (1)
+    {
+        // {LeonHuang20180313+ [Write GPS data and Direction light data] 
+        // {Direction light+ 
+        if (TMR0IF) 
         {
-            
-            for(i=0; i < 100;i++)
+            timerCount++;
+            if (timerCount >= 313) // 312 + 1, 20 ms 
             {
-                putch(GPS[i]);
-              //  if(GPS[i+1]==0)
-                    if(GPS[i]==0x0A)
-                    break;
+                checkFrequencyRange();
+                timerCount = 0;
+                TMR0 = 126; //  128 - 2 
             }
-            
-            if(i<10)
-               __delay_ms(19); //j= 1;
-            
-            if( (i>=10) && (i<20))
-               __delay_ms(18); //j= 2;
-            
-            if( (i>=20) && (i<30))
-               __delay_ms(17); //j= 3;
-            
-            if( (i>=30) && (i<40))
-               __delay_ms(16); //j= 4;
-            
-            if( (i>=40) && (i<50))
-               __delay_ms(15); //j= 5;
-            
-            if( (i>=50) && (i<60))
-               __delay_ms(14); //j= 6;
-            
-            if( (i>=60) && (i<70))
-               __delay_ms(13); //j= 7;
-            
-            if( (i>=70) && (i<80))
-               __delay_ms(12); //j= 8;
-            
-            if( (i>=80) && (i<100))
-               __delay_ms(11);//j= 9;
-            
-            for(i=0; i < 100;i++)
-                GPS[i]= 0;
-
-            GPS_flag = 0;
-            GPS_out = 0;
-            //k= 20-j;
-            //__delay_ms(k); 
+            TMR0IF = 0;
         }
-        else
-           __delay_ms(20);  
-         
-         
-        if( g_Direction == 0)
-            sensorVal_1 =0;    
-        else
-            sensorVal_1 =1;
-        checkFrequencyRange(lowValueOfFrequency, highValueOfFrequency, sensorVal_1); 
+        // Direction light-}
         
-}
+        // Need 30 ~ 79 ms to output a completed GPS data
+        while (outputCount > 0) // Have a completed GPS data in buffer  
+        {
+            putch(buffer[(wHead + wIndex) % SIZE]);
+            
+            //  End of GPS data 
+            if (buffer[(wHead + wIndex) % SIZE] == 0x0A)
+            {
+                highestPriority = 1;
+                outputCount--;
+                wHead = (wHead + wIndex + 1) % SIZE; // new GPS data 
+                wIndex = 0;
+                highestPriority = 0;
+                break;
+            }
+            wIndex++;
+            
+            // Timer overflow detect 
+            if (TMR0IF) 
+            {
+                timerCount++;
+                TMR0IF = 0;
+            }
+        }
+        
+        if (timerCount >= 313) // 312 + 1, 20ms 
+        {
+            checkFrequencyRange();
+            timerCount = 0;
+            TMR0 = 126; //  128 - 2
+        }
+        // LeonHuang20180313-}
+        
+    // {LeonHuang20180313+ [ Original code, hot key: Ctrl + / ] 
+//        if (outputCount == 1)
+//        {
+//            for (i = 0; i < 100; i++)
+//            {
+//                putch(buffer[i]);
+//                    if (buffer[i] == 0x0A)
+//                    break;
+//            }
+//            
+//            if (i<10)
+//                __delay_ms(19); // j= 1;
+//            
+//            if ((i >= 10) && ( i < 20))
+//                __delay_ms(18); // j= 2;
+//            
+//            if( (i>=20) && (i<30))
+//               __delay_ms(17); //j= 3;
+//            
+//            if( (i>=30) && (i<40))
+//               __delay_ms(16); //j= 4;
+//            
+//            if( (i>=40) && (i<50))
+//               __delay_ms(15); //j= 5;
+//            
+//            if( (i>=50) && (i<60))
+//               __delay_ms(14); //j= 6;
+//            
+//            if( (i>=60) && (i<70))
+//               __delay_ms(13); //j= 7;
+//            
+//            if( (i>=70) && (i<80))
+//               __delay_ms(12); //j= 8;
+//            
+//            if( (i>=80) && (i<100))
+//               __delay_ms(11);//j= 9;
+//            
+//            for (i = 0; i < 100; i++)
+//                buffer[i]= 0;
+//
+//            index = 0;
+//            outputCount = 0;
+//        }
+//        else
+//        {
+//            __delay_ms(20);  
+//        }
+//            
+//        if ( g_Direction == 0)
+//            sensorVal_1 = 0;    
+//        else
+//            sensorVal_1 = 1;
+//        
+//        checkFrequencyRange(lowValueOfFrequency, highValueOfFrequency, sensorVal_1); 
+        // LeonHuang20180313-}
+    }
 }
 //================================================================================================
 //  _____                           _        ____            _                   ___       _ _   
@@ -258,87 +421,86 @@ void Stop_turning_LR(){
    }
 }
 */
-
-void checkFrequencyRange(float lowValue, float highValue, int sensorVal)
+// {LeonHuang20180313+ [Optimization] 
+/* Timer0 20ms Setup 
+ 1. Processor frequency: 16MHz， 16M source edges per second
+ 2. Internal instruction cycle clock = Fosc / 4 = 16MHz / 4 = 4MHz
+ 3. 4M Internal instruction cycle per second, therefore 20ms has 80K Internal instruction cycle
+ 4. OPTION_REG, PSA = 1 means TMR0 rate = 1 : 1
+ 5. one Internal instruction cycle -> TMR0 + 1
+ 6. When TMR0 overflow (>255) -> INTCON, TMR0IF = 1
+ 80K / 256 = 312... 128 -> overflow 312 times, TMR0 = 128 (means it pass 20ms)
+ TMR0 = 126 // 128 - 2 (Write data to TMR0 need 2 instruction cycle)
+ */
+void initTimer0()
 {
-  float countForHighValue = 33;//((1 / highValue) * 1000 / 20);
-  float countForLowValue = 83;//((1/ lowValue) * 1000 / 20);
+    TMR0 = 126; // 128 - 2 
+    OPTION_REGbits.T0CS = 0; // Internal instruction cycle clock (FOSC/4)
+    OPTION_REGbits.PSA = 1; // If PSA = 1 (Prescaler Assigned to Watchdog Timer), TMR0 Rate = 1:1
+    INTCONbits.T0IF = 0;
+}
 
-  // Count
-  if (sensorVal == 0)
-  {
-    Neg[0] = Neg[0] + 1;
-    if (Pos[0] > 0)
-      Pos[1] = 1;     
-  }
-  else
-  {
-    Pos[0] = Pos[0] + 1;
-    if (Neg[0] > 0)
-      Neg[1] = 1;
-  }
-
-  // Clear data
-  if (Pos[0] > countForLowValue)
-  {
-    Pos[0]= 0;
-    Period[0]= 0; 
-  }
-  if (Neg[0] > countForLowValue)
-  {
-    Neg[0]= 0;
-    Period[1]= 0; 
-  }
-
-  // Record count information for HIGH
-  if (Pos[1] == 1)
-  {
-//    Serial.print('%');
-//    Serial.println(Pos[0], DEC);
-    Period[0] = Pos[0]; 
-    Pos[0] = 0;
-    Pos[1] = 0; 
-  }
-
-  // Record count information for LOW
-  if (Neg[1] == 1)
-  {
-//    Serial.print('%');
-//    Serial.println(Neg[0], DEC);
-    Period[1] = Neg[0];
-    Neg[0] = 0;
-    Neg[1] = 0;
-  }
-
-  // Check frequency range
-  counter++;
-  if (counter >= 10) // 200 ms
-  {
-    Period[2] = Period[0] + Period[1];
-
-
-   // if ((Period[2] >= 33) && (Period[2] <= 83))
-    
-    if ((Period[2] >= countForHighValue) && (Period[2] <= countForLowValue))
+void checkFrequencyRange()
+{
+    // Count positive and negative 
+    if (g_Direction == 0)
     {
-      putch('%');
-      putch('0');
- putch('\r');
-  putch('\n');
-      //putch(g_Direction);
+        tempNegativeVal += 1;
+
+        if (tempPositiveVal > 0)
+        {
+            positiveVal = tempPositiveVal;
+            tempPositiveVal = 0;
+        }
     }
     else
     {
-      putch('%');
-      putch('1');   
-  putch('\r');
-  putch('\n');
- 
-      //putch(g_Direction);
+        tempPositiveVal += 1;
+    
+        if (tempNegativeVal > 0) 
+        {
+            negativeVal = tempNegativeVal;
+            tempNegativeVal = 0;
+        }
+    }
+    
+    // Clear data
+    if (tempNegativeVal > UPPER_BOUND) // > 83 count
+    {
+        tempNegativeVal = 0;
+        negativeVal = 0;
+    }
+    if (tempPositiveVal > UPPER_BOUND) // > 83 count
+    {
+        tempPositiveVal = 0;
+        positiveVal = 0;
     }
 
-    counter = 0;
-  }
+    // Check once per 200 ms
+    freqCount++;
+    if (freqCount >= 10) // 200 ms
+    {
+        unsigned char sum = negativeVal + positiveVal;
+        if ((sum >= LOWER_BOUND) && (sum <= UPPER_BOUND))
+        {
+            putch('%');
+            putch('0');
+            putch('\r');
+            putch('\n');
+            //putch(g_Direction);
+        }
+        else
+        {
+            putch('%');
+            putch('1');   
+            putch('\r');
+            putch('\n');
+            //putch(g_Direction);
+        }
+
+        freqCount = 0;
+    }
 }
+// LeonHuang20180313-}
 /// @endcond
 
